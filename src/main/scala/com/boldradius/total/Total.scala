7aa6d2466738860893b3233e82e5c6c1dd55b26d
Patrick Premont <patrick.premont@boldradius.com>
2014-12-10 17:22:49 +0000

    Documented API.

--- src/main/scala/com/boldradius/total/Total.scala
+++ src/main/scala/com/boldradius/total/Total.scala
@@ -15,29 +15,163 @@
 */
 package com.boldradius.total
 
+package ops {
+
+trait Extension[K <: AnyId, +V] {
+  val total: Total[V] {type Id >: K <: AnyId}
+  val newIds: Seq[total.Id]
+
+  def mapKeys(f: Seq[total.Id] => Seq[total.Id]): Extension[K, V] =
+    Extension[K, V](total)(f(newIds))
+}
+
+trait SingleExtension[K <: AnyId, +V] extends Extension[K, V] {
+  final val newIds = List(newId)
+  val newId: total.Id
+}
+
+object Extension {
+  def apply[K <: AnyId, V](total_ : Total[V] {type Id >: K <: AnyId})(keys_ : Seq[total_.Id]) = new Extension[K, V] {
+    val total: total_.type = total_
+    val newIds: Seq[total.Id] = keys_
+  }
+}
+
+object SingleExtension {
+  def apply[K <: AnyId, V](total_ : Total[V] {type Id >: K <: AnyId})(key_ : total_.Id) = new SingleExtension[K, V] {
+    val total: total_.type = total_
+    val newId: total.Id = key_
+  }
+}
+
+trait Contraction[K <: AnyId, +V] {
+  val total: Total[V] {type Id <: K}
+
+  def removed: Seq[(K, V)]
+
+  def filter(k: K): Option[total.Id]
+}
+
+trait SingleContraction[K <: AnyId, +V] extends Contraction[K, V] {
+  val total: Total[V] {type Id <: K}
+  val removedKey: K
+  val removedValue: V
+
+  def removed = List((removedKey, removedValue))
+
+  override def filter(k: K) =
+    if (k.v == removedKey.v) None
+    else Some(k.asInstanceOf[total.Id]) // This cast performed for performance reasons. O(nesting-level) proof seams too slow.
+}
+
+}
+
+import ops._
+
+
 /**
  * An immutable indexable collection of values of type `V`. To index in the collection,
  * the apply method must be called with an identifier of the member type ``Id``.
  * Insertion in the collection produces a new collection with a different `Id` type that
  * is a supertype of the original `Id` type. Conversely removal from the collection
  * produces a new collection with an `Id` type that is a subtype or the original.
- * @tparam V
+ * @tparam V The type of the elements in the collection
  */
 sealed trait Total[+V] {
   /**
    * The type for identifiers that may be used to index in the collection.
    */
   type Id <: AnyId
+  /**
+   * The number of elements in the collection. This size is accessible in constant time.
+   */
   val size : Int
-  def apply(k : Id) : V
+
+  /**
+   * Indexes in the collection.
+   * @param id Any identifier in the `Id` type of this collection.
+   * @return The value in the collection that corresponds to the id.
+   *         The existence of this value within the collection is guaranteed,
+   *         so no exceptions will ever be thrown due to an invalid id
+   *         (unless null is passed in).
+   */
+  def apply(id : Id) : V
+
+  /**
+   * Narrows the type of a specific id down to the id in the collection ``Id`` type.
+   * Since it is a narrowing conversion, it may fail, in which case None is returned.
+   * @param id Any id
+   * @return The same id, converted to the collection's `Id` type, or None if the
+   *         id is not part of the collection.
+   */
   def narrowId(id: Id2[_, _, _]) : Option[Id]
+
+  /**
+   * Applies a function to every element of the collection and produces a
+   * new collection containing the resulting values, in corresponding
+   * positions (under the same id). The Id type is unchanged.
+   * @param f The function to be applied to all the elements
+   * @tparam V2 The type of the resulting elements
+   * @return The new ``Total``, with the same `Id` type.
+   */
   def map[V2](f: V => V2): Total[V2] {type Id = Total.this.Id}
-  def update[V2 >: V](k: Id, f: V2 => V2): Total[V2] {type Id = Total.this.Id}
+  /**
+   * Updates a particular element of the collection. Applies a function to this
+   * element and produces a new collection where the element is replaced by the
+   * result of the function application. The Id type is unchanged.
+   * @param id The id of the element to be replaced
+   * @param f The function to be applied to element at ``id``
+   * @tparam V2 The type of the resulting elements
+   * @return The new ``Total``, with the same `Id` type.
+   */
+  def update[V2 >: V](id: Id, f: V2 => V2): Total[V2] {type Id = Total.this.Id}
+
+  /**
+   * Adds an element to the collection. An id is automatically allocated,
+   * according to some unspecified order. The resulting `Total` has
+   * an `Id` type which is a supertype of the original `Id` type, so any
+   * values of the old `Id` type that were kept in other data structures,
+   * may simply be upcasted to the new `Id` type.
+   * @param value The value to be inserted in the collection
+   * @tparam V2 The type of the value to insert, which will be the value type of the
+   *            resulting collection.
+   * @return A `SingleExtension` which captures the two (dependent) results:
+   *         the resulting Total, and the newly allocated id
+   */
   def insert[V2 >: V](value: V2): SingleExtension[Id, V2]
-  def split[A, B](f: V => Either[A, B]) : (TotalSub[Id, A], TotalSub[Id, B])
+
+  /**
+   * Partitions a `Total` in two according to a classification function.
+   * The function classifies each value of the collection for transfer to
+   * one of the two results. The ids of classified values remain the same.
+   * The `Id` types of the two resulting `Total` are both subtypes of
+   * original `Id` type.
+   * @param f The function that classified each element as a `Left` or a `Right`
+   * @tparam A The value type of the ``Total`` for the `Left` class.
+   * @tparam B The value type of the ``Total`` for the `Left` class.
+   * @return A tuple of the two resulting ``Total`` (left and right).
+   */
+  def partition[A, B](f: V => Either[A, B]) : (TotalSub[Id, A], TotalSub[Id, B])
+
+  /**
+   * A stream of all the elements in the collection, as tuples of id and value.
+   * @return A stream of all the elements in the collection, as tuples of id and value.
+   */
   def toStream : Stream[(Id, V)]
   private[total] def removeInner(removedKey: Id) : (TotalSub[Id, V], V)
 
+  /**
+   * Adds a sequence of elements to the collection. A corresponding number of
+   * ids are automatically allocated, according to some unspecified order.
+   * The resulting `Total` has an `Id` type which is a supertype of the
+   * original `Id` type, so any values of the old `Id` type that were kept
+   * in other data structures, may simply be upcasted to the new `Id` type.
+   * @param values The values to be inserted in the collection
+   * @tparam V2 The type of the values to insert, which will be the value type of the
+   *            resulting collection.
+   * @return An `Extension` which captures the two (dependent) results:
+   *         the resulting Total, and the newly allocated ids (in the same order)
+   */
   def insertAll[V2 >: V](values: Seq[V2]): Extension[Id, V2] = {
     values.foldLeft[Extension[Id, V2]](
       Extension[Id, V2](this)(Nil)) {
@@ -46,6 +180,16 @@ sealed trait Total[+V] {
         Extension[Id, V2](singleExtension.total)(singleExtension.newId +: extension.newIds): Extension[Id, V2]
     }.mapKeys(_.reverse)
   }
+  /**
+   * Removes an element from the collection, at a specific id. The resulting `Total` has
+   * an `Id` type which is a subtype of the original `Id` type. Any
+   * values of the old `Id` type that were kept in other data structures,
+   * may be narrowed down with the returned filtering function, which may be more efficient
+   * that ``narrowId``.
+   * @param removedKey The identifier for element to be removed.
+   * @return A `SingleContraction` which captures the (dependent) results:
+   *         the resulting Total, the removed value, the removed key, and the filtering function
+   */
   def remove(removedKey: Id): SingleContraction[Id, V] = {
     val removedInner = removeInner(removedKey)
     val k = removedKey
@@ -56,8 +200,26 @@ sealed trait Total[+V] {
       val removedKey = k
     }
   }
-  def filter(f: V => Boolean) = split(v => if (f(v)) Right(v) else Left(()))  // TODO: Define an optimized version, split rebuilds the whole tree
-  def keep[V2](f: V => Option[V2]) = split(f(_).toRight(()))
+
+  /**
+   * Filters the collections according to a predicate. All and only the elements for which the
+   * predicate returns true will be in the resulting total. The `Id` type of the result
+   * is a subtype of the original `Id` type. NOTE It is often preferable to use `keep`
+   * instead of `filter`, because it allows the resulting collection to have a distinct value type,
+   * which is typically narrower, and reflects what was learned as a result of running the test function.
+   * @param p The predicate function, which is true for elements to be included
+   * @return The new collection with a subset of the original elements.
+   */
+  def filter(p: V => Boolean) = partition(v => if (p(v)) Right(v) else Left(()))  // TODO: Define an optimized version, partition rebuilds the whole tree
+  /**
+   * Keeps all elements that can be converted to a new type with a provided
+   * test function. The function returns an `Option` of the new type, which is
+   * `None` when elements fail the test and must be discarded. The `Id` type of the result
+   * is a subtype of the original `Id` type.
+   * @param test The test function, which returns ``Some`` for elements to be included, and ``None`` otherwise
+   * @return The new collection with a subset of the original elements.
+   */
+  def keep[V2](test: V => Option[V2]) = partition(test(_).toRight(()))
 }
 private[total] case object TotalNothing extends Total[Nothing] {
   type Id = Nothing
@@ -72,7 +234,7 @@ private[total] case object TotalNothing extends Total[Nothing] {
     SingleExtension[Id, V2](i.total)(i.newId)
   }
   def removeInner(key: Nothing) = key
-  def split[A, B](f: Nothing => Either[A, B]) : (TotalSub[Id, A], TotalSub[Id, B]) = (Total.empty, Total.empty)
+  def partition[A, B](f: Nothing => Either[A, B]) : (TotalSub[Id, A], TotalSub[Id, B]) = (Total.empty, Total.empty)
   def toStream = Stream.empty
 }
 private[total] case class TotalWith[+V, K1 <: AnyId, K2 <: AnyId](v: V, t1: Total[V] {type Id = K1}, t2: Total[V] {type Id = K2}) extends Total[V] {
@@ -114,9 +276,9 @@ private[total] case class TotalWith[+V, K1 <: AnyId, K2 <: AnyId](v: V, t1: Tota
         val (t2a, removed) = t2.removeInner(k2)
         (TotalWith[V, t1.Id, t2a.Id](v, t1, t2a), removed)
       })
-  def split[A, B](f: V => Either[A, B]) : (TotalSub[Id, A], TotalSub[Id, B]) = {
-    val s1 = t1.split(f)
-    val s2 = t2.split(f)
+  def partition[A, B](f: V => Either[A, B]) : (TotalSub[Id, A], TotalSub[Id, B]) = {
+    val s1 = t1.partition(f)
+    val s2 = t2.partition(f)
     f(v).fold(
       a => (TotalWith(a, s1._1, s2._1), TotalWithout(s1._2, s2._2)),
       b => (TotalWithout(s1._1, s2._1), TotalWith(b, s1._2, s2._2)))
@@ -162,9 +324,9 @@ private[total] case class TotalWithout[+V, K1 <: AnyId, K2 <: AnyId](t1: Total[V
         val (t2a, v) = t2.removeInner(k2)
         (TotalWithout[V, t1.Id, t2a.Id](t1, t2a), v)
       })
-  def split[A, B](f: V => Either[A, B]) : (TotalSub[Id, A], TotalSub[Id, B]) = {
-    val s1 = t1.split(f)
-    val s2 = t2.split(f)
+  def partition[A, B](f: V => Either[A, B]) : (TotalSub[Id, A], TotalSub[Id, B]) = {
+    val s1 = t1.partition(f)
+    val s2 = t2.partition(f)
     (TotalWithout(s1._1, s2._1), TotalWithout(s1._2, s2._2))
   }
   def toStream : Stream[(Id, V)] = // TODO: revisit method name, the order is surprising. Improve algorithm
@@ -175,40 +337,3 @@ object Total {
   val empty = TotalNothing
 }
 
-trait Extension[K <: AnyId, +V] {
-  val total: Total[V] {type Id >: K <: AnyId}
-  val newIds : Seq[total.Id]
-  def mapKeys(f: Seq[total.Id] => Seq[total.Id]) : Extension[K, V] =
-    Extension[K, V](total)(f(newIds))
-}
-trait SingleExtension[K <: AnyId, +V] extends Extension[K, V] {
-  final val newIds = List(newId)
-  val newId : total.Id
-}
-object Extension {
-  def apply[K <: AnyId, V](total_ : Total[V] {type Id >: K <: AnyId})(keys_ : Seq[total_.Id]) = new Extension[K, V] {
-    val total : total_.type = total_
-    val newIds : Seq[total.Id] = keys_
-  }
-}
-object SingleExtension {
-  def apply[K <: AnyId, V](total_ : Total[V] {type Id >: K <: AnyId})(key_ : total_.Id) = new SingleExtension[K, V] {
-    val total : total_.type = total_
-    val newId : total.Id = key_
-  }
-}
-
-trait Contraction[K <: AnyId, +V] {
-  val total: Total[V] {type Id <: K}
-  def removed : Seq[(K, V)]
-  def filter(k: K) : Option[total.Id]
-}
-trait SingleContraction[K <: AnyId, +V] extends Contraction[K, V] {
-  val total: Total[V] {type Id <: K}
-  val removedKey : K
-  val removedValue : V
-  def removed = List((removedKey, removedValue))
-  override def filter(k: K) =
-    if (k.v == removedKey.v) None
-    else Some(k.asInstanceOf[total.Id]) // This cast performed for performance reasons. O(nesting-level) proof seams too slow.
-}
