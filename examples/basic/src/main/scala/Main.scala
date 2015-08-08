15d05cf3ba5682cc3d727a030ab818348aee82f4
Patrick Premont <patrick.premont@boldradius.com>
2015-08-08 17:10:25 -0400

    Added a benchmark for inserts in basic example

--- examples/basic/src/main/scala/Main.scala
+++ examples/basic/src/main/scala/Main.scala
@@ -1,14 +1,32 @@
 import com.boldradius.total._
 
 object Main {
-  def main(a: Array[String]) = {
+  def main(a: Array[String]) : Unit = {
 
-    val added = Total.empty.insertAll(0 to 1000000)
+    //val added = Total.empty.insertAll(0 to 1000000)
+    //val removed = added.total.remove(added.newIds.head)
+    //println(removed.total.toStream.take(100).toList)
 
-    val removed = added.total.remove(added.newIds.head)
+    println(List(100000, 1000000).map(size =>
+      (size,
+        benchmarkNanos(Total.empty.insertAll(0 to size))/size*1e6f,
+        //benchmarkNanos((1 to size).map(i => (i, i)).toMap : Map[Int, Int]),
+        benchmarkNanos((1 to size).map(i => (i, i))(scala.collection.breakOut) : Map[Int, Int])/size*1e6f
+        )))
+  }
 
-    println(removed.total
-      .toStream.take(100).toList)
-
+  def benchmarkNanos(task: => Unit, warmupNanos : Long = 10*1000*1000*1000, runNanos : Long = 10*1000*1000*1000, runs: List[Long] = Nil) : Float = {
+    val nanos = timeNanos(task)
+    if (warmupNanos > 0) benchmarkNanos(task, warmupNanos - nanos, runNanos, runs)
+    else if (runNanos > 0) benchmarkNanos(task, 0, runNanos - nanos, nanos :: runs)
+    else (nanos :: runs).map(_.toFloat).average * 1e-9f
+  }
+  implicit class FloatSeqOps(v: Seq[Float]) {
+    def average = v.sum / v.length
+  }
+  def timeNanos(task: => Unit) : Long = {
+    val start = System.nanoTime()
+    task
+    System.nanoTime() - start
   }
 }
